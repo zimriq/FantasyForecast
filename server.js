700a6a038dadbec832628e1b6450c3ddb30ce49f
Zimri Quinonez <zimryquinonez@gmail.com>
2026-02-27 12:17:28 -0500

    finished compare api route, next add cache

--- server.js
+++ server.js
@@ -5,18 +5,48 @@ const cors = require('cors');
 const app = express(); 
 const PORT = process.env.PORT || 3000;  //letting render assign dynamic port 
 
-
+app.use(cors());
 app.use(express.json()); 
 app.use(express.static('public')); 
 
-//api/scores
-
-//api/player/:name
+//helper functs
+async function getSleeperPlayers() {
+    const response = await axios.get('https://api.sleeper.app/v1/players/nfl');
+    const players = Object.values(response.data);
+    return players;
+}
 
 //api/compare
+const ALLOWED_POSITIONS = ['QB', 'RB', 'WR', 'TE'];
+app.get('/api/compare', async (req, res) => {
+    const {player1, player2} = req.query;
+    if(!player1 || !player2) {
+        return res.status(400).json({error: 'Both player names are required'});
+    }
 
+    try{
+        const players = await getSleeperPlayers(); 
+        const p1 = players.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player1.toLowerCase());
+        const p2 = players.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player2.toLowerCase()); 
+
+        if(!p1 || !p2) {
+            return res.status(404).json({error: 'One or both players were not found'});
+        }
+        return res.json({ player1: p1, player2: p2 });
+    } catch(error) {
+        console.error('Sleeper API error:', error.message);
+        return res.status(502).json({error: 'Failed to fetch player data from upstream service.'});
+    }
+});
 
 //api/test-defense
+app.get('/api/test-defense', async (req, res) => {
+    try{
+
+    }catch(error) {
+        console.error('')
+    }
+});
 
 
 app.listen(PORT, () => {
