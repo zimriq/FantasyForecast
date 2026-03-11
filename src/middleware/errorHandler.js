a4b8078d76fefac7607d67690aa6a5196392a4f1
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-11 13:57:30 -0400

    finished error handling file

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+
+const errorHandler = ((err, req, res, next) => {
+	console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(status).json({ error: message}); 
+});
+
+module.exports = {errorHandler};
