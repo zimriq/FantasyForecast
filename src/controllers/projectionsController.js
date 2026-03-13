88eca37a2f0363548b7617da380f8d7eae5dff8c
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-13 11:07:11 -0400

    started proj controllers, and finished getting player proj funct

---
+++ src/controllers/projectionsController.js
@@ -0,0 +1,34 @@
+const sleeperService = require('../services/sleeperService');
+const {ALLOWED_POSITIONS} = require('../utils/constant');
+
+const getPlayerProjections = async (req, res, next) => {
+    try{
+        const {player1, player2, season, week} = req.query;
+
+        if(!player1 || !player2) {
+        return res.status(400).json({error: 'Both player names are required'});
+    }
+
+        const playerList = await sleeperService.getSleeperPlayers();
+        const p1 = playerList.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player1.toLowerCase());
+        const p2 = playerList.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player2.toLowerCase()); 
+        if(!p1 || !p2) {
+            return res.status(404).json({error: 'One or both players not found'});
+        }
+
+        const proj = await sleeperService.getProjections(season, week); 
+
+        const p1Proj = proj.find(p => p.player_id === p1.player_id);
+        const p2Proj = proj.find(p => p.player_id === p2.player_id); 
+
+        res.json({
+            player1Proj: p1Proj,
+            player2Proj: p2Proj
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {getPlayerProjections}; 
+
