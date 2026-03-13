6b52244bccad3e75455df02766cae728e695404b
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-13 18:00:31 -0400

    server merge completed and imports added

--- server.js
+++ server.js
@@ -1,35 +1,5 @@
-//api/compare
-app.get('/api/compare', async (req, res) => {
-    const {player1, player2} = req.query;
-    if(!player1 || !player2) {
-        return res.status(400).json({error: 'Both player names are required'});
-    }
-
-    try{
-        const players = await getSleeperPlayers(); 
-        const p1 = players.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player1.toLowerCase());
-        const p2 = players.find(p => ALLOWED_POSITIONS.includes(p.position) && p.status === "Active" && p.full_name?.toLowerCase() === player2.toLowerCase()); 
-
-        if(!p1 || !p2) {
-            return res.status(404).json({error: 'One or both players were not found'});
-        }
-        return res.json({ player1: p1, player2: p2 });
-    } catch(error) {
-        console.error('Sleeper API error:', error.message);
-        return res.status(502).json({error: 'Failed to fetch player data from upstream service.'});
-    }
-});
-
-//api/matchup
-app.get('/api/matchup', async (req, res) => {
-    try{
-
-
-    }catch(error) {
-        console.error('')
-    }
-});
-
+const app = require('./src/app');
+const {PORT} = require('./src/config/env');
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
