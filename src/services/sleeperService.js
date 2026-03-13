eded6656537b5a8fa370ead04da1cc42e1a36e1d
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-13 19:06:06 -0400

    updating template literals

--- src/services/sleeperService.js
+++ src/services/sleeperService.js
@@ -15,7 +15,7 @@ async function getProjections(season, week) {
     const cached = projectionsCache.get('players_proj'); 
     if(cached) return cached; 
 
-    const response = await axios.get('https://api.sleeper.app/v1/projections/nfl/regular/<season>/<week>'); 
+    const response = await axios.get('https://api.sleeper.app/v1/projections/nfl/regular/${season}/${week}'); 
     const proj = Object.values(response.data); 
     projectionsCache.set('players_proj', proj); 
     return proj; 
@@ -25,7 +25,7 @@ async function getDefMatchup() {
     const cached = defMatchupCache.get('defense_matchup'); 
     if(cached) return cached; 
 
-    const response = await axios.get('https://api.sleeper.app/v1/regular/<season>/<week>'); 
+    const response = await axios.get('https://api.sleeper.app/v1/regular/${season}/${week}'); 
     const defM = Object.values(response.data); 
     defMatchupCache.set('defense_matchup', defM);
     return defM;
