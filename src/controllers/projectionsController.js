022cd2e5322941b7dc2e82f9ba1fa05e74c05364
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-27 10:40:37 -0400

    Adding player opponents, getting def stats, and matching them to player opponents

--- src/controllers/projectionsController.js
+++ src/controllers/projectionsController.js
@@ -20,13 +20,23 @@ const getPlayerProjections = async (req, res, next) => {
         }
 
         const proj = await sleeperService.getProjections(season, week); 
-
         const p1Proj = proj.find(p => p.player_id === p1.player_id);
         const p2Proj = proj.find(p => p.player_id === p2.player_id); 
 
+        const matchups = await sleeperService.getESPNMatchups(season, week); 
+        const p1Opp = matchups[p1.team]; 
+        const p2Opp = matchups[p2.team]; 
+
+        const defStats = await sleeperService.getDefMatchup(season, week); 
+        const defTeams = defStats.filter(d => isNaN(d.player_id) && !d.player_id.startsWith('TEAM_')); 
+        const p1Defense = defTeams.find( d => d.player_id === p1Opp); 
+        const p2Defense = defTeams.find(d => d.player_id === p2Opp); 
+
         res.json({
             player1Proj: p1Proj,
-            player2Proj: p2Proj
+            player2Proj: p2Proj,
+            p1Defense, 
+            p2Defense
         });
     } catch (err) {
         next(err);
