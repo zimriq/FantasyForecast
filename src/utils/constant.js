495ad1a00e83d9f0eeb5f808344df2e2a63ee667
Zimri Quinonez <zimryquinonez@gmail.com>
2026-03-27 11:03:38 -0400

    creating a const field map for positions linking them to defense fantasy points allowed to that pos

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,9 @@
 const ALLOWED_POSITIONS = ['QB', 'RB', 'WR', 'TE'];
-module.exports = { ALLOWED_POSITIONS }; 
+const ALLOWED_FIELD_MAP = {
+    'WR': 'fan_pts_allow_wr',
+    'RB': 'fan_pts_allow_rb',
+    'QB': 'fan_pts_allow_qb',
+    'TE': 'fan_pts_allow_te'
+};
+
+module.exports = { ALLOWED_POSITIONS, ALLOWED_FIELD_MAP}; 
