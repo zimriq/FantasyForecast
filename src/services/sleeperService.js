59de566d0d9744b0c1c3a67fbd47feaa3c688215
Zimri Quinonez <zimryquinonez@gmail.com>
2026-04-20 09:39:38 -0400

    editing axios import

--- src/services/sleeperService.js
+++ src/services/sleeperService.js
@@ -1,4 +1,4 @@
-const axios = require('axios'); 
+const axios = require('../config/axiosInstance');
 const { playerCache, projectionsCache, defMatchupCache, getESPNMatchupsCache } = require('../utils/cache'); 
 
 async function getSleeperPlayers() {
